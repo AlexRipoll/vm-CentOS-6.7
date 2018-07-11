6c4356409aa5665591e3efbb47866717e580eab4
AlexRP <alexripollpiera@gmail.com>
2018-07-11 20:36:13 +0200

    Añadiendo carpeta de archivos compartidos y carpeta Ansible con el playbook y los demas archivos de configuracion

---
+++ ansible/templates/index.php
@@ -0,0 +1,9 @@
+
+<html>
+
+<body>
+ <h1>
+ 	Hello World. Sistema operativo  
+ 	{{ ansible_distribution }} 	{{ ansible_distribution_major_version}};
+ </h1>
+</body>
---
+++ shared/web/index.php
@@ -0,0 +1,9 @@
+
+<html>
+
+<body>
+ <h1>
+ 	Hello World. Sistema operativo  
+ 	CentOS 	7;
+ </h1>
+</body>
